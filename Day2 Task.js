4ec92edebfd0e6edc6377c7cb326acc9aaa637b4
jay1patel1 <jayp.fsd@outlook.com>
2025-04-13 17:55:16 +0530

    Chat gpt task is done

---
+++ Day2 Task.js
@@ -0,0 +1,8 @@
+function greet (name, age) {
+    const message = `Hello ${name}, your age is: ${age}`;
+    return message;
+}
+
+console.log(greet("Jay",32));
+console.log(greet("Arti",30));
+console.log(greet("Krishnakant",60));
