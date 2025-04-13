a38a5d5ba2555e67ff63fabc72af98c94b6aef25
jay1patel1 <jayp.fsd@outlook.com>
2025-04-13 18:03:34 +0530

    Chat gpt task is done

--- Day2 Task.js
+++ Day2 Task.js
@@ -1,8 +1,18 @@
 function greet (name, age) {
-    const message = `Hello ${name}, your age is: ${age}`;
+    let message = `Hello ${name}, your age is: ${age}.`;
+
+    if (age < 18) {
+        message += " You are young! You can play but don't able to vote.";
+    } else if (age >= 18 && age <= 60) {
+        message += " You will be able to vote";
+    } else {
+        message += " You are old relax you don't need to vote";
+    }
     return message;
 }
 
-console.log(greet("Jay",32));
+console.log(greet("Mishti",12));
 console.log(greet("Arti",30));
+console.log(greet("Jay",18));
 console.log(greet("Krishnakant",60));
+console.log(greet("Geeta",61));
